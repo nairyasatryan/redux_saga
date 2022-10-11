b26774e3e15664e593cbfe3a9382668115472f61
nairyasatryan <nairyasatryan@gmail.com>
2022-10-11 15:37:30 +0400

    Redux-Saga

---
+++ src/store.js
@@ -0,0 +1,34 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import watchSaga from "./saga";
+
+const dataSlice = createSlice({
+  name: "Name",
+  initialState: {
+    fact: "",
+  },
+  reducers: {
+    fetchData: (state, action) => {
+      console.log(state, action);
+      return {
+        ...state,
+        fact: action.payload,
+      };
+    },
+  },
+});
+
+ export const { fetchData } = dataSlice.actions;
+console.log(dataSlice);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: {
+    catFact: dataSlice.reducer,
+  },
+  middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(watchSaga);
+
+export default store;
